4c14ba1b8f687e5e6655277f270c633005b3d395
taipun <taipun2000@hotmail.com>
2020-04-08 20:54:06 +0700

    [UPDATE]Basic Application Structure

---
+++ lxapp/lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:lxapp/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    // return either the Home or Authenticate widget
+    return Home();
+    
+  }
+}
