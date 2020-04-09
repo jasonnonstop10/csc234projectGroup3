fbd765463fb7594261b56df75fc3f452adb16905
taipun <taipun2000@hotmail.com>
2020-04-09 13:15:55 +0700

    [ADD]Provider package

--- lxapp/lib/screens/wrapper.dart
+++ lxapp/lib/screens/wrapper.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:lxapp/models/user.dart';
 import 'package:lxapp/screens/authenticate/authenticate.dart';
 import 'package:lxapp/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
+    final user = Provider.of<User>(context);
+    print(user);
     // return either the Home or Authenticate widget
     return Authenticate();
     
