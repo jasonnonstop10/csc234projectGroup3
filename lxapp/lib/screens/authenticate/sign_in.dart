df64d1a32b695fdb0680890d76a5e17a4bfdd04c
taipun <taipun2000@hotmail.com>
2020-04-09 14:56:20 +0700

    [UPDATE]Sign in with email
    Register
    Sign in
    Verification
    Stream provider
    Work Perfectly
    Test Account Email:taipun2000@hotmail.com
                         Password:12345678

--- lxapp/lib/screens/authenticate/sign_in.dart
+++ lxapp/lib/screens/authenticate/sign_in.dart
@@ -10,11 +10,11 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
-
+  final _formKey = GlobalKey<FormState>();
    // text field state
   String email = '';
   String password = '';
-
+  String error = '';
    @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,16 +36,19 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+            key: _formKey,
             child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.length < 8 ? 'Enter the password' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -59,9 +62,18 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if(_formKey.currentState.validate()){
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if(result == null) {
+                      setState(() => error = 'invalid credentials');
+                     }
+                  }
                 }
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
               ),
             ],
           ),
