6f65e1fc65c532173f603febf0ad35c27ced0330
taipun <taipun2000@hotmail.com>
2020-04-09 13:05:01 +0700

    [ADD]Streams to auth file

--- lxapp/lib/services/auth.dart
+++ lxapp/lib/services/auth.dart
@@ -9,7 +9,12 @@ class AuthService {
   User _userFromFirebaseUser(FirebaseUser user) {
     return user != null ? User(uid: user.uid) : null;
   }
-
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+      .map(_userFromFirebaseUser);
+  }
   // sign in anon
   Future signInAnon() async {
     try {
